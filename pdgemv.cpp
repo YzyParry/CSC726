a886dc5c27fd31ad3e81e0caf29e6072502406d4
Parry Yang <yangz223@wfu.edu>
2024-12-03 17:15:27 -0500

    add toggle to create global identity matrix

--- pdgemv.cpp
+++ pdgemv.cpp
@@ -8,6 +8,7 @@ const bool DEBUG = true;
 
 // initialize matrix and vectors (A is mxn, x is xn-vec)
 void init_rand(double* a, int m, int n, double* x, int xn);
+void init_identity(double* a, int m, int n, double* x, int xn, int ranki, int rankj);
 // local matvec: y = y+A*x, where A is m x n
 void local_gemv(double* A, double* x, double* y, int m, int n);
 
@@ -21,15 +22,20 @@ int main(int argc, char** argv) {
     srand(rank*12345);
 
     // Read dimensions and processor grid from command line arguments
-    if(argc != 5) {
-        cerr << "Usage: ./a.out rows cols pr pc" << endl;
-        return 1;
-    }
+    // if(argc != 5) {
+    //     cerr << "Usage: ./a.out rows cols pr pc" << endl;
+    //     return 1;
+    // }
     int m, n, pr, pc;
     m  = atoi(argv[1]);
     n  = atoi(argv[2]);
     pr = atoi(argv[3]);
     pc = atoi(argv[4]);
+
+    //  toggles between constructing a random matrix and a (global) identity matrix
+    int toggle;
+    toggle = atoi(argv[5]);
+
     if(pr*pc != nProcs) {
         cerr << "Processor grid doesn't match number of processors" << endl;
         return 1;
@@ -65,7 +71,12 @@ int main(int argc, char** argv) {
     double* Alocal = new double[mloc*nloc];
     double* xlocal = new double[xdim];
     double* ylocal = new double[ydim];
-    init_rand(Alocal, mloc, nloc, xlocal, xdim);
+    if (toggle) {
+        init_identity(Alocal, mloc, nloc, xlocal, xdim, ranki, rankj);
+    } else {
+        init_rand(Alocal, mloc, nloc, xlocal, xdim);
+    }
+    
     memset(ylocal,0,ydim*sizeof(double));
 
     // start timer
@@ -127,12 +138,12 @@ int main(int argc, char** argv) {
     MPI_Scatter(yglobal,ydim,MPI_DOUBLE,ylocal,ydim,MPI_DOUBLE,0,row_comm);
 
     MPI_Barrier(MPI_COMM_WORLD);
-    if (rank<6) {
-        cout << "\nRank " << rank << "Proc (" << ranki << "," << rankj << ") started with y values\n";
-        for(int i = 0; i < ydim; i++) {
-            cout << ylocal[i] << " ";
-        }
-    }
+    // if (rank<6) {
+    //     cout << "\nRank " << rank << "Proc (" << ranki << "," << rankj << ") started with y values\n";
+    //     for(int i = 0; i < ydim; i++) {
+    //         cout << ylocal[i] << " ";
+    //     }
+    // }
     
 
     int receiver = pc * (rank % pr) + (rank / pr);
@@ -143,35 +154,35 @@ int main(int argc, char** argv) {
     // Stop timer
     MPI_Barrier(MPI_COMM_WORLD);
 
-    if (rank<6) {
-        cout << "\nproc" << rank << " send to " << receiver << " and rec from " << sender << endl;
-        cout << "\nRank " << rank << "Proc (" << ranki << "," << rankj << ") ended with y values\n";
-        for(int i = 0; i < ydim; i++) {
-            cout << yglobal[i] << " ";
-        }
-    }
+    // if (rank<6) {
+    //     cout << "\nproc" << rank << " send to " << receiver << " and rec from " << sender << endl;
+    //     cout << "\nRank " << rank << "Proc (" << ranki << "," << rankj << ") ended with y values\n";
+    //     for(int i = 0; i < ydim; i++) {
+    //         cout << yglobal[i] << " ";
+    //     }
+    // }
 
     time = MPI_Wtime() - start;
 
     // Print results for debugging
     if(DEBUG) {
-        // cout << "\nRank " << rank << " Proc (" << ranki << "," << rankj << ") started with x values\n";
-        // for(int j = 0; j < xdim; j++) {
-        //     cout << xlocal[j] << " ";
-        // }
+        cout << "\nRank " << rank << " Proc (" << ranki << "," << rankj << ") started with x values\n";
+        for(int j = 0; j < xdim; j++) {
+            cout << xlocal[j] << " ";
+        }
 
-        // cout << "\nProc (" << ranki << "," << rankj << ") has local matrix\n";
-        // for (int i = 0; i < mloc; i++) {
-        //     for (int j=0; j < nloc; j++) {
-        //         cout << Alocal[j*mloc + i] << " ";
-        //     }
-        //     cout << endl;
-        // }
+        cout << "\nProc (" << ranki << "," << rankj << ") has local matrix\n";
+        for (int i = 0; i < mloc; i++) {
+            for (int j=0; j < nloc; j++) {
+                cout << Alocal[j*mloc + i] << " ";
+            }
+            cout << endl;
+        }
         
-        // cout << "\nRank " << rank << "Proc (" << ranki << "," << rankj << ") ended with y values\n";
-        // for(int i = 0; i < ydim; i++) {
-        //     cout << yglobal[i] << " ";
-        // }
+        cout << "\nRank " << rank << "Proc (" << ranki << "," << rankj << ") ended with y values\n";
+        for(int i = 0; i < ydim; i++) {
+            cout << yglobal[i] << " ";
+        }
         cout << endl; // flush now
     }
 
@@ -205,6 +216,23 @@ void init_rand(double* a, int m, int n, double* x, int xn) {
     for(int i = 0; i < m; i++) {
         for(int j = 0; j < n; j++) {
             a[i+j*m] = rand() % 100;
+        }
+    }
+    // init input vector x
+    for(int j = 0; j < xn; j++) {
+        x[j] = rand() % 100;
+    }
+}
+
+void init_identity(double* a, int m, int n, double* x, int xn, int ranki, int rankj) {
+    // init a (global) identity matrix
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            // Calculate global row and column indices
+            int global_row = ranki * m + i;
+            int global_col = rankj * n + j;
+            // Set local matrix elements: 1 if diagonal, 0 otherwise
+            a[i+j*m] = (global_row == global_col) ? 1 : 0;
         }
     }
     // init input vector x
