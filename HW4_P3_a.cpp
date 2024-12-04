f653aa00c784c65d7ee0ca163c1aeffbf8ca7ef8
Parry Yang <yangz223@wfu.edu>
2024-12-04 15:54:49 -0500

    finished, ready for the benchmark

--- HW4_P3_a.cpp
+++ HW4_P3_a.cpp
@@ -7,7 +7,9 @@
 #include <mpi.h>
 using namespace std;
 
-const bool PRINT = true;
+const bool PRINT = false;
+const bool CHECK = false;
+
 
 int main(int argc, char** argv) {
     MPI_Init(NULL, NULL);
@@ -30,12 +32,42 @@ int main(int argc, char** argv) {
             global_data[j] = rand() % 100;
         }
     }
-    
     double time, start;
+
     // My_Scatter Implementation
 
-    
+    // For example: 8 proc
+    // step 1: 0->4
+    // step 2: 0->2, 4->6
+    // step 3: 0->1, 2->3, 4->5, 6->7
+    int data_size;
+    if (rank == 0) {
+        data_size = global_data_size;
+    } else {
+        data_size = 0;
+    }
 
+    for (int i = nProcs; i > 1; i >>= 1) {
+        int dest, source;
+
+        if (rank % i == 0) { // Sender
+            dest = rank + (i >> 1);
+            // cout << rank << " send msg to " << dest << " at i=" << i << endl;
+            int send_count = data_size / 2;
+            int offset = data_size - send_count;
+            MPI_Send(global_data + offset, send_count, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
+            // // Keep only the first half
+            data_size = offset;
+            
+        } else if (rank % i == (i >> 1)) { // Receiver
+            source = rank - (i >> 1);
+            // cout << rank << " recv msg from " << source << " at i=" << i << endl;
+            data_size = (global_data_size * i) / (2 * nProcs);
+            int recv_count = data_size;
+            MPI_Recv(global_data, recv_count, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+    }
+    memcpy(localdata, global_data, local_data_size * sizeof(double));
 
     // MPI_Scatter() verifies the correctness
     start = MPI_Wtime();
@@ -46,7 +78,6 @@ int main(int argc, char** argv) {
     if (rank == 0) {
         cout << "MPI_Scatter: " << time << " seconds" << endl;
     }
-
 
     if (PRINT) {
         if (rank == 0)
@@ -63,6 +94,21 @@ int main(int argc, char** argv) {
             cout << localdata[i] << " ";
         }
         cout << endl;
+
+        cout << "\nRank " << rank << " should end with values\n";
+        for(int i = 0; i < local_data_size; i++) {
+            cout << localdata_ref[i] << " ";
+        }
+        cout << endl;
+    }
+
+    if (CHECK) {
+        for (int i = 0; i < local_data_size; i++)
+        {
+            if (localdata[i]!=localdata_ref[i]) {
+                cerr << "Does not match the MPI_Scatter!" << endl;
+            }
+        }
     }
 
     MPI_Finalize();
