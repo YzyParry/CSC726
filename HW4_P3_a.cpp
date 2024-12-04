aa197a3ce795d9e03b796c8192d1b1f7cc588f5c
Parry Yang <yangz223@wfu.edu>
2024-12-04 11:08:22 -0500

    create new files

---
+++ HW4_P3_a.cpp
@@ -0,0 +1,9 @@
+// Homework 4 problem 3 question (a)
+// Implement the binomial tree recursive halving algorithm for Scatter.
+
+#include <iostream>
+#include <cstring>
+#include <stdlib.h>
+#include <mpi.h>
+using namespace std;
+
