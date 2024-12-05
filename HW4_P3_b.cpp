0b6754223d341cbe6806fe5586ffd6a5263d2641
Parry Yang <yangz223@wfu.edu>
2024-12-05 17:35:17 -0500

    Finish Implementation

--- HW4_P3_b.cpp
+++ HW4_P3_b.cpp
@@ -28,7 +28,8 @@ int main(int argc, char** argv) {
     double* global_data_ref =  new double[global_data_size];
 
     for(int j = 0; j < local_data_size; j++) {
-        localdata[j] = rand() % 100;
+        global_data[j] = rand() % 100;
+        localdata[j] = global_data[j];
     }
 
     double time, start;
@@ -36,16 +37,47 @@ int main(int argc, char** argv) {
     // My_Allgather Implementation
 
     start = MPI_Wtime();
-
-    for (int i = 1; i < nProcs; i>>=1)
+    int offset, dest, source, data_size;
+    for (int i = 1; i < nProcs; i<<=1)
     {
-        MPI_Sendrecv();
+        int dest, source;
+        int data_size = m * i;
+        int send_offset, recv_offset;
+
+        if ((rank % (i << 1)) < i) {
+            dest = rank + i;
+            source = rank + i;
+            send_offset = 0;
+            recv_offset = data_size; // 收到的数据接在后面
+        } else {
+            dest = rank - i;
+            source = rank - i;
+            send_offset = 0;
+            recv_offset = 0; // 收到的数据应该放在前面
+        }
+
+        if ((rank % (i << 1)) >= i) {
+            double* temp = new double[data_size];
+            MPI_Sendrecv(global_data + send_offset, data_size, MPI_DOUBLE, dest, 0,
+                         temp, data_size, MPI_DOUBLE, source, 0,
+                         MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            // 当前 global_data 前 data_size 个元素是自己的数据，
+            // 先把自己的数据段挪到后面，腾出前面的位置给 temp 中的 partner 数据。
+            memmove(global_data + data_size, global_data, data_size * sizeof(double));
+            memcpy(global_data, temp, data_size * sizeof(double));
+            delete[] temp;
+        } else {
+            MPI_Sendrecv(global_data + send_offset, data_size, MPI_DOUBLE, dest, 0,
+                         global_data + recv_offset, data_size, MPI_DOUBLE, source, 0,
+                         MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
     }
     
 
     time = MPI_Wtime() - start;
     if (rank == 0) {
-        cout << "My_Scatter: " << time << " seconds" << endl;
+        cout << "My_Allgather: " << time << " seconds" << endl;
     }
 
 
@@ -56,7 +88,7 @@ int main(int argc, char** argv) {
    
     time = MPI_Wtime() - start;
     if (rank == 0) {
-        cout << "MPI_Scatter: " << time << " seconds" << endl;
+        cout << "MPI_Allgather: " << time << " seconds" << endl;
     }
 
     if (PRINT) {
@@ -67,11 +99,11 @@ int main(int argc, char** argv) {
         }
         cout << endl;
 
-        // cout << "\nRank " << rank << " ended with values\n";
-        // for(int i = 0; i < global_data_size; i++) {
-        //     cout << global_data[i] << " ";
-        // }
-        // cout << endl;
+        cout << "\nRank " << rank << " ended with values\n";
+        for(int i = 0; i < global_data_size; i++) {
+            cout << global_data[i] << " ";
+        }
+        cout << endl;
 
         cout << "\nRank " << rank << " should end with values\n";
         for(int i = 0; i < global_data_size; i++) {
@@ -80,14 +112,14 @@ int main(int argc, char** argv) {
         cout << endl;
     }
 
-    // if (CHECK) {
-    //     for (int i = 0; i < local_data_size; i++)
-    //     {
-    //         if (localdata[i]!=localdata_ref[i]) {
-    //             cerr << "Does not match the MPI_Scatter!" << endl;
-    //         }
-    //     }
-    // }
+    if (CHECK) {
+        for (int i = 0; i < global_data_size; i++)
+        {
+            if (global_data[i]!=global_data_ref[i]) {
+                cerr << "Does not match the MPI_Scatter!" << endl;
+            }
+        }
+    }
 
     MPI_Finalize();
 }
